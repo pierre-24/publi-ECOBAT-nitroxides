8dccf6f4e4049e0a101ef76217d888b3d2e49dcd
pierre-24 <pierreb24@gmail.com>
2024-06-02 22:59:52 +0200

    starts to considers cation-radical interaction

--- analyses/plot_CIP.py
+++ analyses/plot_CIP.py
@@ -25,15 +25,14 @@ R = 8.3145
 T= 298.15
 F =  9.64853321233100184e4
 
-def Ef_ox(X: float, k01: float, k02: float, k12: float, E0: float = 0):
-    return E0 + R * T / F * numpy.log((1+k12*X**2) / (1+k01*X+k02*X**2))
+def Ef_ox(X: float, k01: float, k02: float, k11: float, k12: float, E0: float = 0):
+    return E0 + R * T / F * numpy.log((1+k11*X+k12*X**2) / (1+k01*X+k02*X**2))
     
-def Ef_red(X: float, k12: float, k21: float, k22: float, E0: float = 0):
-    return E0 + R * T / F * numpy.log((1+k21*X+k22*X**2) / (1+k12*X**2))
+def Ef_red(X: float, k11: float, k12: float, k21: float, k22: float, E0: float = 0):
+    return E0 + R * T / F * numpy.log((1+k21*X+k22*X**2) / (1+k11*X+k12*X**2))
 
-for kx1, kx2, color in [(1, 1e-3, 'tab:blue'), (1e-3, 1, 'tab:orange'), (1, 1e-1, 'tab:green'), (1e-1, 1, 'tab:red')]:
-    ax.plot(X, Ef_ox(X, kx1, kx2, kx2), color=color, label='$K_{{x1}}$={}, $K_{{x2}}={}$'.format(kx1, kx2))
-    ax.plot(X, Ef_red(X, kx2, kx1, kx2), '--', color=color)
+for ki1, kip1, ki2, kip2, color in [(1, 1e-2, 1,1, 'tab:green'), (1e-2, 1, 1e-2, 1, 'tab:blue'), (1, 1e-2, 1, 1e-2, 'tab:red'), (1, 1, 1, 1e-2, 'tab:orange')]:
+    ax.plot(X, Ef_ox(X, ki1, ki2, kip1, kip2), color=color, label='$K_{{01}}$={}, $K_{{11}}$={}, $K_{{02}}$={}, $K_{{12}}$={}'.format(ki1, kip1, ki2, kip2))
 
 ax.set_xlabel('[X] (mol L$^{-1}$)')
 ax.set_ylabel('$E^f_{abs}$ (V)')
